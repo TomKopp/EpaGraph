22c0651990a404dd733f8e117b033bd29c1565d4
Tom Kopp <witzbould@live.com>
2017-12-29 14:46:48 +0100

    update adjMatrix

--- Graph/adjMatrix.h
+++ Graph/adjMatrix.h
@@ -2,14 +2,14 @@
 
 #include "stdafx.h"
 
-typedef struct graph {
+typedef struct Graph {
 	int** adj; /**< Adjacency matrix. */
 	int n; /**< Number of nodes in graph. */
-} Graph;
+};
 
-Graph *createGraph(int nodes) {
-	int r = nodes, c = nodes, i, j, count;
-	Graph * tmp = (Graph*)malloc(sizeof(Graph));
+Graph * createGraph(int nodes) {
+	int r = nodes, c = nodes, i, j;
+	Graph * tmp = (Graph *)malloc(sizeof(Graph));
 
 	tmp->adj = (int **)malloc(sizeof(int *) * r);
 	tmp->adj[0] = (int *)malloc(sizeof(int) * c * r);
@@ -19,10 +19,8 @@ Graph *createGraph(int nodes) {
 	}
 
 	tmp->n = nodes;
-	//count = 0;
 	for (i = 0; i < r; i++) {
 		for (j = 0; j < c; j++) {
-			//tmp->adj[i][j] = ++count;
 			tmp->adj[i][j] = 0;
 		}
 	}
@@ -30,16 +28,18 @@ Graph *createGraph(int nodes) {
 	return tmp;
 }
 
-void printAdjMatrix(Graph * g) {
+void printGraph(Graph * g) {
 	int r = g->n, c = g->n, i, j;
 
+	printf("\n");
 	for (i = 0; i < r; i++) {
 		printf("\n");
 		for (j = 0; j < c; j++) {
 			printf("%d\t", g->adj[i][j]);
 		}
 	}
-	printf("\n");
+
+	return;
 }
 
 void addEdge(Graph * g, int v1, int v2) {
@@ -57,7 +57,7 @@ void removeEdge(Graph * g, int v1, int v2) {
 }
 
 bool testEdge(Graph * g, int v1, int v2) {
-	if (g->adj[v1][v2] > 0)
+	if (v1 < g->n && v2 < g->n && g->adj[v1][v2] > 0)
 		return true;
 
 	return false;
